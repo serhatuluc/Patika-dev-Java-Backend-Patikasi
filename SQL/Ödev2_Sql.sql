22997857afde95aa4d933483eb78775f71607e73
serhatuluc <99399280+serhatuluc@users.noreply.github.com>
2022-07-10 00:07:12 +0300

    Update Ödev2_Sql.sql

--- SQL/Ödev2_Sql.sql
+++ SQL/Ödev2_Sql.sql
@@ -2,7 +2,7 @@
 select * from film
 where rental_rate in (0.99,2.99,4.99) and replacement_cost in (12.99,15.99,28.99)
 
-2.Soru
+--2.Soru
 select first_name,last_name from actor
 where first_name in ('Penelope', 'Nick','Ed')
 
